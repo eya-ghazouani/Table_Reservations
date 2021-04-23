f4b879d735b4ccc0c0b34c61c3f4c70eabcd9a95
eya-ghazouani <eyaghazouani160@gmail.com>
2021-04-24 00:53:11 +0200

    first_commit

--- database/migrations/2021_04_23_192714_create_reservations_table.php
+++ database/migrations/2021_04_23_192714_create_reservations_table.php
@@ -16,6 +16,11 @@ class CreateReservationsTable extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('client_name',30);
+            $table->date('date_arrivee');
+            $table->integer('nbr_nuits');
+            $table->integer('nbr_chambres');
+            $table->integer('nbr_adultes');
+            $table->integer('nbr_enfants');
             $table->timestamps();
         });
     }
